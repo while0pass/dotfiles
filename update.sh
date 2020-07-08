ac4dd26b5adf99ad483c4285b13ede25132b7b2a
Nurono <while0pass@yandex.ru>
2020-07-08 21:58:33 +0300

    bugfix: для обновления tmux-плагинов требуется аргумент

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ sudo apt -y autoremove
 sudo snap refresh
 
 ~/.tmux/plugins/tpm/bin/clean_plugins
-~/.tmux/plugins/tpm/bin/update_plugins
+~/.tmux/plugins/tpm/bin/update_plugins all
 ~/.tmux/plugins/tpm/bin/install_plugins
 
 python -m pip install -U pip pipenv tmuxp
