5f72dba20b5c7f981772291b35b43bca19ab97ba
Nurono <while0pass@yandex.ru>
2022-10-31 00:24:16 +0300

    Fix keyboard layout display in tmux

--- bash/show_keyboard_layout.sh
+++ bash/show_keyboard_layout.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 layout=$(
 
-  gdbus call \
-      --session \
-      --dest org.gnome.Shell \
-      --object-path /org/gnome/Shell \
-      --method org.gnome.Shell.Eval \
-      'imports.ui.status.keyboard.getInputSourceManager().currentSource.id' \
-  | cut -d'"' -f2
+# gdbus call \
+#     --session \
+#     --dest org.gnome.Shell \
+#     --object-path /org/gnome/Shell \
+#     --method org.gnome.Shell.Eval \
+#     'imports.ui.status.keyboard.getInputSourceManager().currentSource.id' \
+# | cut -d'"' -f2
+
+  gsettings get org.gnome.desktop.input-sources mru-sources | cut -f4 -d"'"
 
 )
 echo ${layout^^}
